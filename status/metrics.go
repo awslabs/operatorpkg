7a218db1f9bd6e3d7f3889fa42e5a18591467240
Ellis Tarn <ellistarn@gmail.com>
2023-09-25 17:47:16 -0700

    feat: Added test libraries and some object utils (#5)

--- status/metrics.go
+++ status/metrics.go
@@ -1,58 +1,9 @@
 package status
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/awslabs/operatorpkg/object"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/samber/lo"
-	controllerruntime "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
-
-type Controller struct {
-	kubeClient client.Client
-	forObject  ConditionedObject
-}
-
-func NewController(client client.Client, forObject ConditionedObject) *Controller {
-	return &Controller{
-		kubeClient: client,
-		forObject:  forObject,
-	}
-}
-
-func (c *Controller) Register(ctx context.Context, m manager.Manager) error {
-	return controllerruntime.NewControllerManagedBy(m).
-		For(c.forObject).
-		Named(fmt.Sprintf("metrics.%s", object.GVK(c.forObject).Kind)).
-		Complete(c)
-}
-
-func (c *Controller) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
-	o := c.forObject.DeepCopyObject().(ConditionedObject)
-	labels := prometheus.Labels{
-		metricLabelKind:      object.GVK(o).Kind,
-		metricLabelNamespace: req.Namespace,
-		metricLabelName:      req.Name,
-	}
-	ConditionCount.DeletePartialMatch(labels)
-
-	if err := c.kubeClient.Get(ctx, req.NamespacedName, o); err != nil {
-		return reconcile.Result{}, client.IgnoreNotFound(err)
-	}
-
-	for _, condition := range o.GetConditions() {
-		ConditionCount.MustCurryWith(labels).With(prometheus.Labels{
-			metricLabelConditionType:   condition.Type,
-			metricLabelConditionStatus: string(condition.Status),
-		}).Set(1)
-	}
-	return reconcile.Result{}, nil
-}
 
 func RegisterMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(
---
+++ test/environment.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"github.com/samber/lo"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+type Environment struct {
+	envtest.Environment
+	Client              client.Client
+	KubernetesInterface kubernetes.Interface
+}
+
+type EnvironmentOption func(env *envtest.Environment)
+
+func EnvironmentOptionWithCRDs(crds []*apiextensionsv1.CustomResourceDefinition) func(env *envtest.Environment) {
+	return func(env *envtest.Environment) {
+		env.CRDs = crds
+	}
+}
+
+func NewEnvironment(options ...EnvironmentOption) *Environment {
+	environment := envtest.Environment{Scheme: scheme.Scheme}
+	for _, option := range options {
+		option(&environment)
+	}
+
+	config := lo.Must(environment.Start())
+	return &Environment{
+		Client:              lo.Must(client.New(config, client.Options{Scheme: scheme.Scheme})),
+		KubernetesInterface: kubernetes.NewForConfigOrDie(config),
+	}
+}
