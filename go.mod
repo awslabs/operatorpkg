e3a22161c260afa45922bcab610513d641229f74
Ellis Tarn <ellistarn@gmail.com>
2024-03-15 15:06:31 -0700

    feat: Improve condition message when propagated to root  condition (#16)

--- go.mod
+++ go.mod
@@ -4,18 +4,22 @@ go 1.20
 
 require (
 	github.com/Pallinder/go-randomdata v1.2.0
+	github.com/go-logr/logr v1.2.4
+	github.com/go-logr/zapr v1.2.4
 	github.com/imdario/mergo v0.3.6
 	github.com/onsi/ginkgo/v2 v2.11.0
 	github.com/onsi/gomega v1.27.10
 	github.com/prometheus/client_golang v1.16.0
 	github.com/prometheus/client_model v0.4.0
 	github.com/samber/lo v1.38.1
-	gopkg.in/yaml.v2 v2.4.0
+	go.uber.org/zap v1.25.0
 	k8s.io/api v0.28.2
 	k8s.io/apiextensions-apiserver v0.28.0
 	k8s.io/apimachinery v0.28.2
 	k8s.io/client-go v0.28.2
+	k8s.io/klog/v2 v2.100.1
 	sigs.k8s.io/controller-runtime v0.16.2
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
@@ -26,7 +30,6 @@ require (
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
@@ -50,6 +53,7 @@ require (
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
@@ -62,12 +66,11 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/component-base v0.28.1 // indirect
-	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- status/condition_set_test.go
+++ status/condition_set_test.go
@@ -17,7 +17,7 @@ var _ = Describe("Conditions", func() {
 		Expect(conditions.Get(ConditionTypeFoo).GetStatus()).To(Equal(metav1.ConditionUnknown))
 		Expect(conditions.Get(ConditionTypeBar).GetStatus()).To(Equal(metav1.ConditionUnknown))
 		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionUnknown))
-		// Update the condition
+		// Update the condition to true
 		conditions.SetTrue(ConditionTypeFoo)
 		fooCondition := conditions.Get(ConditionTypeFoo)
 		Expect(fooCondition.Type).To(Equal(ConditionTypeFoo))
@@ -25,27 +25,27 @@ var _ = Describe("Conditions", func() {
 		Expect(fooCondition.Reason).To(Equal(ConditionTypeFoo))  // default to type
 		Expect(fooCondition.Message).To(Equal(ConditionTypeFoo)) // default to type
 		Expect(fooCondition.LastTransitionTime.UnixNano()).To(BeNumerically(">", 0))
-		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionUnknown)) // Root condition is still unknown
+		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionUnknown))
 		time.Sleep(1 * time.Nanosecond)
-		// Update the other condition
-		conditions.SetTrueWithReason(ConditionTypeBar, "reason", "message")
+		// Update the other condition to false
+		conditions.SetFalse(ConditionTypeBar, "reason", "message")
 		fooCondition2 := conditions.Get(ConditionTypeBar)
 		Expect(fooCondition2.Type).To(Equal(ConditionTypeBar))
-		Expect(fooCondition2.Status).To(Equal(metav1.ConditionTrue))
+		Expect(fooCondition2.Status).To(Equal(metav1.ConditionFalse))
 		Expect(fooCondition2.Reason).To(Equal("reason"))
 		Expect(fooCondition2.Message).To(Equal("message"))
 		Expect(fooCondition.LastTransitionTime.UnixNano()).To(BeNumerically(">", 0))
-		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionTrue)) // Root condition automatically set to true
+		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionFalse))
 		time.Sleep(1 * time.Nanosecond)
-		// transition the condition
-		conditions.SetFalse(ConditionTypeFoo, "reason", "message")
-		updatedFooCondition := conditions.Get(ConditionTypeFoo)
-		Expect(updatedFooCondition.Type).To(Equal(ConditionTypeFoo))
-		Expect(updatedFooCondition.Status).To(Equal(metav1.ConditionFalse))
+		// transition the root condition to true
+		conditions.SetTrueWithReason(ConditionTypeBar, "reason", "message")
+		updatedFooCondition := conditions.Get(ConditionTypeBar)
+		Expect(updatedFooCondition.Type).To(Equal(ConditionTypeBar))
+		Expect(updatedFooCondition.Status).To(Equal(metav1.ConditionTrue))
 		Expect(updatedFooCondition.Reason).To(Equal("reason"))
 		Expect(updatedFooCondition.Message).To(Equal("message"))
 		Expect(updatedFooCondition.LastTransitionTime.UnixNano()).To(BeNumerically(">", fooCondition.LastTransitionTime.UnixNano()))
-		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionFalse)) // Root condition automatically set to false
+		Expect(conditions.Root().GetStatus()).To(Equal(metav1.ConditionTrue))
 		time.Sleep(1 * time.Nanosecond)
 		// Transition if the status is the same, but the Reason is different
 		conditions.SetFalse(ConditionTypeBar, "another-reason", "another-message")
