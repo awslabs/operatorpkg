70c68e1a80f0b2018ede2a2a25585b3ba5f2b826
njtran <nichotr@amazon.com>
2024-05-13 14:03:19 -0700

    rename

--- test/expectations/expectations.go
+++ test/expectations/expectations.go
@@ -84,7 +84,7 @@ func ExpectApplied(ctx context.Context, c client.Client, objects ...client.Objec
 	}
 }
 
-func ExpectObjectStatus(ctx context.Context, c client.Client, obj status.Object, conditions ...status.Condition) {
+func ExpectStatus(ctx context.Context, c client.Client, obj status.Object, conditions ...status.Condition) {
 	GinkgoHelper()
 	objStatus := obj.StatusConditions()
 	for _, cond := range conditions {
