4f0c6c6047b4a0ee5aebc776ee4dda48e2b5baec
Jonathan Innis <joinnis@amazon.com>
2024-09-10 18:31:39 -0700

    Update to use a TypedRateLimiter

--- reasonable/reasonable.go
+++ reasonable/reasonable.go
@@ -6,11 +6,12 @@ import (
 
 	"golang.org/x/time/rate"
 	"k8s.io/client-go/util/workqueue"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func RateLimiter() workqueue.RateLimiter {
-	return workqueue.NewMaxOfRateLimiter(
-		workqueue.NewItemExponentialFailureRateLimiter(100*time.Millisecond, 1*time.Minute),
-		&workqueue.BucketRateLimiter{Limiter: rate.NewLimiter(rate.Limit(10), 100)},
+func RateLimiter() workqueue.TypedRateLimiter[reconcile.Request] {
+	return workqueue.NewTypedMaxOfRateLimiter[reconcile.Request](
+		workqueue.NewTypedItemExponentialFailureRateLimiter[reconcile.Request](100*time.Millisecond, 1*time.Minute),
+		&workqueue.TypedBucketRateLimiter[reconcile.Request]{Limiter: rate.NewLimiter(rate.Limit(10), 100)},
 	)
 }
