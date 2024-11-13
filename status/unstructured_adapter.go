3b4d980925bcbda62d458e6f46063a678d6a9fa9
Amanuel Engeda <aengeda@amazon.com>
2024-11-12 18:03:16 -0800

    Add object specific Metric

--- status/unstructured_adapter.go
+++ status/unstructured_adapter.go
@@ -7,22 +7,34 @@ import (
 	"github.com/samber/lo"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // unstructuredAdapter is an adapter for the status.Object interface. unstructuredAdapter
 // makes the assumption that status conditions are found on status.conditions path.
-type unstructuredAdapter struct {
+type unstructuredAdapter[T client.Object] struct {
 	unstructured.Unstructured
 }
 
-func NewUnstructuredAdapter(obj client.Object) *unstructuredAdapter {
+func NewUnstructuredAdapter[T client.Object](obj client.Object) *unstructuredAdapter[T] {
 	u := unstructured.Unstructured{Object: lo.Must(runtime.DefaultUnstructuredConverter.ToUnstructured(obj))}
-	u.SetGroupVersionKind(object.GVK(obj))
-	return &unstructuredAdapter{Unstructured: u}
+	ua := &unstructuredAdapter[T]{Unstructured: u}
+	ua.SetGroupVersionKind(object.GVK(obj))
+	return ua
 }
 
-func (u *unstructuredAdapter) GetConditions() []Condition {
+func (u *unstructuredAdapter[T]) GetObjectKind() schema.ObjectKind {
+	return u
+}
+func (u *unstructuredAdapter[T]) SetGroupVersionKind(gvk schema.GroupVersionKind) {
+	u.Unstructured.SetGroupVersionKind(gvk)
+}
+func (u *unstructuredAdapter[T]) GroupVersionKind() schema.GroupVersionKind {
+	return object.GVK(object.New[T]())
+}
+
+func (u *unstructuredAdapter[T]) GetConditions() []Condition {
 	conditions, _, _ := unstructured.NestedSlice(u.Object, "status", "conditions")
 	return lo.Map(conditions, func(condition interface{}, _ int) Condition {
 		var newCondition Condition
@@ -32,7 +44,7 @@ func (u *unstructuredAdapter) GetConditions() []Condition {
 		return newCondition
 	})
 }
-func (u *unstructuredAdapter) SetConditions(conditions []Condition) {
+func (u *unstructuredAdapter[T]) SetConditions(conditions []Condition) {
 	unstructured.SetNestedSlice(u.Object, lo.Map(conditions, func(condition Condition, _ int) interface{} {
 		var b map[string]interface{}
 		j, _ := json.Marshal(&condition)
@@ -41,7 +53,7 @@ func (u *unstructuredAdapter) SetConditions(conditions []Condition) {
 	}), "status", "conditions")
 }
 
-func (u *unstructuredAdapter) StatusConditions() ConditionSet {
+func (u *unstructuredAdapter[T]) StatusConditions() ConditionSet {
 	conditionTypes := lo.Map(u.GetConditions(), func(condition Condition, _ int) string {
 		return condition.Type
 	})
