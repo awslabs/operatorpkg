531af49a79c447f5e7f4af077a731f51a69f38b7
Amanuel Engeda <aengeda@amazon.com>
2025-01-24 11:29:37 -0800

    Removed the source queue and pulled object from the channel

--- singleton/controller.go
+++ singleton/controller.go
@@ -2,9 +2,13 @@ package singleton
 
 import (
 	"context"
+	"math"
 	"time"
 
+	"go.uber.org/multierr"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/util/workqueue"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -24,6 +28,38 @@ type Reconciler interface {
 func AsReconciler(reconciler Reconciler) reconcile.Reconciler {
 	return reconcile.Func(func(ctx context.Context, r reconcile.Request) (reconcile.Result, error) {
 		return reconciler.Reconcile(ctx)
+	})
+}
+
+type ChannalObjectReconciler[T client.Object] interface {
+	Reconcile(ctx context.Context, object T) (reconcile.Result, error)
+}
+
+func AsChannelObjectReconciler[T client.Object](watchEvents <-chan watch.Event, reconciler ChannalObjectReconciler[T]) reconcile.Reconciler {
+	return reconcile.Func(func(ctx context.Context, r reconcile.Request) (reconcile.Result, error) {
+		var errs error
+		var results []reconcile.Result
+		for event := range watchEvents {
+			res, err := reconciler.Reconcile(ctx, event.Object.(T))
+			errs = multierr.Append(errs, err)
+			results = append(results, res)
+
+		}
+
+		var result reconcile.Result
+		min := time.Duration(math.MaxInt64)
+		for _, r := range results {
+			if r.IsZero() {
+				continue
+			}
+			if r.RequeueAfter < min {
+				min = r.RequeueAfter
+				result.RequeueAfter = min
+				result.Requeue = true
+			}
+		}
+
+		return result, errs
 	})
 }
 
