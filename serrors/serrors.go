6a4a581b8019f28659464812a8edd43d70ffc643
Jonathan Innis <joinnis@amazon.com>
2025-04-14 17:36:26 -0700

    Support structured errors for multierr

--- serrors/serrors.go
+++ serrors/serrors.go
@@ -2,12 +2,18 @@ package serrors
 
 import (
 	"errors"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/samber/lo"
+	"go.uber.org/multierr"
 )
 
 // Error is a structured error that stores structured errors and values alongside the error
 type Error struct {
 	error
-	keysAndValues []any
+	keysAndValues map[string]any
 }
 
 // Unwrap returns the unwrapped error
@@ -17,38 +23,75 @@ func (e *Error) Unwrap() error {
 
 // Error returns the string representation of the error
 func (e *Error) Error() string {
-	return e.error.Error()
+	var elems []string
+	keys := lo.Keys(e.keysAndValues)
+	sort.Strings(keys) // sort keys so we always get a consistent ordering
+	for _, k := range keys {
+		v := e.keysAndValues[k]
+		elems = append(elems, fmt.Sprintf("%s=%v", k, v))
+	}
+	return fmt.Sprintf("%s (%s)", e.error.Error(), strings.Join(elems, ", "))
 }
 
 // WithValues injects additional structured keys and values into the error
 func (e *Error) WithValues(keysAndValues ...any) *Error {
-	e.keysAndValues = append(e.keysAndValues, keysAndValues...)
+	lo.Must0(validateKeysAndValues(keysAndValues))
+	if e.keysAndValues == nil {
+		e.keysAndValues = map[string]any{}
+	}
+	for i := 0; i < len(keysAndValues); i += 2 {
+		e.keysAndValues[keysAndValues[i].(string)] = keysAndValues[i+1]
+	}
 	return e
 }
 
 // Wrap wraps and existing error with additional structured keys and values
 func Wrap(err error, keysAndValues ...any) error {
+	e := &Error{error: err}
+	return e.WithValues(keysAndValues...)
+}
+
+func validateKeysAndValues(keysAndValues []any) error {
 	if len(keysAndValues)%2 != 0 {
-		panic("keysAndValues must have an even number of elements")
+		return fmt.Errorf("keysAndValues must have an even number of elements")
 	}
 	for i := 0; i < len(keysAndValues); i += 2 {
 		if _, ok := keysAndValues[i].(string); !ok {
-			panic("keys must be strings")
+			return fmt.Errorf("keys must be strings")
 		}
 	}
-	return &Error{error: err, keysAndValues: keysAndValues}
+	return nil
 }
 
 // UnwrapValues returns a combined set of keys and values from every wrapped error
-func UnwrapValues(err error) (values []any) {
+func UnwrapValues(err error) (res []any) {
+	values := map[string][]any{}
 	for err != nil {
-		if v, ok := err.(*Error); ok {
-			values = append(values, v.keysAndValues...)
+		for _, elem := range multierr.Errors(err) {
+			if e, ok := elem.(*Error); ok {
+				for k, v := range e.keysAndValues {
+					if _, mOk := values[k]; mOk {
+						values[k] = append(values[k], v)
+					} else {
+						values[k] = []any{v}
+					}
+				}
+			}
 		}
 		err = errors.Unwrap(err)
 	}
 	if len(values) == 0 {
 		return nil
 	}
-	return values
+	keys := lo.Keys(values)
+	sort.Strings(keys) // sort keys so we always get a consistent ordering
+	for _, k := range keys {
+		v := values[k]
+		if len(v) == 1 {
+			res = append(res, k, v[0])
+		} else {
+			res = append(res, fmt.Sprintf("%ss", k), v)
+		}
+	}
+	return res
 }
