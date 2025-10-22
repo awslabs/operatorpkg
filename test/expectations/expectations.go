338250a2ff386073d5974afb2065d52697fc4024
ryan-mist <ryanmist@amazon.com>
2025-10-22 10:13:10 -0700

    check namespace object instead of pod

--- test/expectations/expectations.go
+++ test/expectations/expectations.go
@@ -127,6 +127,14 @@ func ExpectApplied(ctx context.Context, c client.Client, objects ...client.Objec
 			}
 		}, "2s", "400ms").Should(Succeed())
 
+		// If the object is a namespace, ensure the default service account exists to avoid flaky tests
+		// https://github.com/kubernetes/kubernetes/issues/66689
+		if namespace, ok := object.(*v1.Namespace); ok {
+			Eventually(func(g Gomega) {
+				g.Expect(c.Get(ctx, apitypes.NamespacedName{Namespace: namespace.Name, Name: "default"}, &v1.ServiceAccount{})).Error().NotTo(HaveOccurred())
+			}).WithTimeout(1 * time.Minute).WithPolling(1 * time.Second).Should(Succeed())
+		}
+
 		// Re-get the object to grab the updated spec and status
 		Expect(c.Get(ctx, client.ObjectKeyFromObject(object), object)).To(Succeed())
 
@@ -135,15 +143,6 @@ func ExpectApplied(ctx context.Context, c client.Client, objects ...client.Objec
 			ExpectDeletionTimestampSet(ctx, c, object)
 		}
 	}
-}
-
-func ExpectServiceAccountForNamespaceCreated(ctx context.Context, c client.Client, pod v1.Pod) {
-	// Ensure the default service account for a pod exists to avoid flaky tests
-	// https://github.com/kubernetes/kubernetes/issues/66689
-	GinkgoHelper()
-	Eventually(func(g Gomega) {
-		g.Expect(c.Get(ctx, apitypes.NamespacedName{Namespace: pod.Namespace, Name: "default"}, &v1.ServiceAccount{})).Error().NotTo(HaveOccurred())
-	}).WithTimeout(1 * time.Minute).WithPolling(1 * time.Second).Should(Succeed())
 }
 
 func getStatus(obj client.Object) interface{} {
